9ef22647902a354a32d49e142b6482ad6e44a531
chroju <chroju@users.noreply.github.com>
2023-11-02 22:59:28 +0900

    Update for my use case

--- src/journal.user.js
+++ src/journal.user.js
@@ -7,16 +7,44 @@ function createJournalItem() {
         WF.showMessage(`Unable to find journal root item`, true);
         return;
     }
-    const todayStr = getTodayString(); // YYYY-MM-DD
-    const monthStr = todayStr.slice(0,7); // YYYY-MM
+    const today = getToday();
+    const todayStr = getTodayString(today);
+   //  const monthStr = getMonthString(today);
+    const wnStr = getWeekNum(today);
     // Create child item
-    WF.editGroup(function() {
+    WF.editGroup(function () {
         // Find the month item
-        const monthItem = findOrCreateItem(journalItem, monthStr);
-        const todayItem = findOrCreateItem(monthItem, todayStr);
+        const weekItem = findOrCreateItem(journalItem, wnStr);
+        const todayItem = findOrCreateItem(weekItem, todayStr);
         // Select the item and put the cursor in the right place
         WF.zoomTo(todayItem);
         WF.editItemName(todayItem);
+    });
+}
+
+function createInterstitialItem() {
+    const journalItem = findTaggedItem(WF.rootItem(), journalItemTag);
+    if (!journalItem) {
+        WF.showMessage(`Unable to find journal root item`, true);
+        return;
+    }
+    const today = getToday();
+    const todayStr = getTodayString(today);
+   //  const monthStr = getMonthString(today);
+    const wnStr = getWeekNum(today);
+    const timeStr = getTimeString(today);
+    // Create child item
+    WF.editGroup(function () {
+        // Find the month item
+        const weekItem = findOrCreateItem(journalItem, wnStr);
+        const todayItem = findOrCreateItem(weekItem, todayStr);
+        const timeItem = findOrCreateItem(todayItem, timeStr);
+        const target = findOrCreateItem(timeItem, '');
+        // Select the item and put the cursor in the right place
+        WF.zoomTo(todayItem);
+        WF.moveItems([timeItem], todayItem, 999);
+        WF.expandItem(timeItem);
+        WF.editItemName(timeItem);
     });
 }
 
@@ -25,8 +53,15 @@ addButton('&#9998;', createJournalItem);
 // Add keyboard shortcut
 document.addEventListener("keydown", function (event) {
     if (!event.altKey && event.shiftKey &&
-            event.ctrlKey && !event.metaKey && event.key === "J") {
+        event.ctrlKey && !event.metaKey && event.key === "J") {
         createJournalItem();
+        event.preventDefault();
+    }
+});
+document.addEventListener("keydown", function (event) {
+    if (!event.altKey && event.shiftKey &&
+        event.ctrlKey && !event.metaKey && event.key === "I") {
+        createInterstitialItem();
         event.preventDefault();
     }
 });
